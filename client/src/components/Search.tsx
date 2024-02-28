3fc0ec0037dc8526f082bee9e16d36d71818b0ef
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-02-28 10:38:01 +0530

    import local data

--- client/src/components/Search.tsx
+++ client/src/components/Search.tsx
@@ -8,19 +8,21 @@ import {
 } from "@/components/ui/command"
 import axios from "axios"
 import { Info } from "./Info"
+import { data } from "@/data"
 
 
 export function Search() {
-    const [items , setItems] = React.useState([])
-    React.useEffect(()=>{
-        async function fetchItems() {
-            await axios.get(`http://localhost:3000/api/v1/item/all`)
-            .then(res =>{
-                setItems(res.data.items)
-            })
-        }
-        fetchItems()
-    },[])
+    const [items , setItems] = React.useState(data)
+    // React.useEffect(()=>{
+    //     async function fetchItems() {
+    //         await axios.get(`http://localhost:3000/api/v1/item/all`)
+    //         .then(res =>{
+    //             setItems(res.data.items)
+    //         })
+    //     }
+    //     fetchItems()
+    // },[])
+  
   return (
         <Command className="bg-black text-white w-1/2">
           <CommandInput placeholder="Search framework..." className="h-10 mt-5" />
